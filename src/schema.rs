cbc2fe8744a657e10398ae9d284734b82c34c10a
andmatcha <jj.mcr185@gmail.com>
2022-09-15 22:55:00 +0900

    スキーマ更新

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,11 @@
 table! {
     posts (id) {
         id -> Integer,
+        created_at -> Nullable<Datetime>,
+        updated_at -> Nullable<Datetime>,
+        deleted_at -> Nullable<Datetime>,
+        title -> Nullable<Varchar>,
+        description -> Nullable<Varchar>,
         body -> Nullable<Text>,
     }
 }
